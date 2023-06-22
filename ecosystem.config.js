33ce595ae60c09d5d49578e1bb22e32f48567764
illi-homz <gopkabel@mail.ru>
2023-06-22 18:25:54 +0300

    add action

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: 'Tatu Laser Remover',
         exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
+        instances: '3', // Or a number of instances
         script: '.output/server/index.mjs',
         args: 'start'
       }
